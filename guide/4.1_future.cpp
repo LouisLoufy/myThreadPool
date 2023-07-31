e73a8c5f9f2b50b76dd63a75558127ba5c4d01df
Louis <louis@deepglint.com>
2023-07-31 21:05:14 +0800

    guide:4 temp

---
+++ guide/4.1_future.cpp
@@ -0,0 +1,28 @@
+/* 主线程获取子线程的返回值 */
+#include <iostream>
+#include <thread>
+#include <future>
+
+void task(int a, int b, std::promise<int>& p) {
+    int retA = a * a;
+    int retB = b * 2;
+
+    p.set_value(retA + retB);
+}
+
+int main() {
+    // 把promise和future做关联
+    std::promise<int> p;
+    std::future<int> f = p.get_future(); 
+
+    std::thread t(task, 1, 2, std::ref(p));
+
+    /* do something else */
+
+    // get the promise value
+    f.wait();
+    // 注意：只能get一次
+    std::cout << "return value is: " << f.get() << std::endl;
+
+    t.join();
+}
---
+++ guide/4.3_shared_future.cpp
@@ -0,0 +1,32 @@
+/* 主线程设置值，子线程获取 */
+#include <iostream>
+#include <thread>
+#include <future>
+
+void task(int a, std::future<int>& b, std::promise<int>& p) {
+    int retA = a * a;
+    int retB = b.get() * 2;
+
+    p.set_value(retA + retB);
+}
+
+int main() {
+    // 把promise和future做关联
+    std::promise<int> p_in;
+    std::future<int> f_in = p_in.get_future();
+
+    std::promise<int> p_ret;
+    std::future<int> f_ret = p_ret.get_future();
+
+    std::thread t(task, 1, std::ref(f_in), std::ref(p_ret));
+
+    /* do something else */
+    p_in.set_value(2);
+
+    // get the promise value
+    f_ret.wait();
+    // 注意：只能get一次
+    std::cout << "return value is: " << f_ret.get() << std::endl;
+
+    t.join();
+}
